4258572e497ae79996bac286c76ab21f2aaa4f6f
Alexey Kuhta <electricsix23@gmail.com>
2024-08-06 23:49:00 +0200

    Algorithm complexity has been reduced to O(n) avg/O(n^2) worst for LZW compression

--- include/compression/LZWCompression.h
+++ include/compression/LZWCompression.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <deque>
 #include <span>
 #include <unordered_map>
 
@@ -7,9 +8,9 @@ namespace cch
 {
     namespace compression
     {
-        struct ByteVectorHash
+        struct ByteDequeHash
         {
-            size_t operator()(std::vector<unsigned char> const& vec)  const
+            size_t operator()(std::deque<unsigned char> const& vec)  const
             {
                 auto hash = vec.size();
 
@@ -24,20 +25,33 @@ namespace cch
             static std::uint32_t const inline magicNumber = 0x9e3779b9;
         };
 
+        struct NoHash
+        {
+            size_t operator()(size_t hash)  const
+            {
+                return hash;
+            }
+        };
+
         class LZWCompression
         {
         public:
             LZWCompression() noexcept;
-            std::vector<size_t> compress(std::span<unsigned char> data);
-            std::vector<unsigned char> decompress(std::span<size_t> data);
+            std::vector<unsigned int> compress(std::span<unsigned char> data);
+            std::vector<unsigned char> decompress(std::span<unsigned int> data);
             void resetState() noexcept;
 
         private:
             void initCompressionDictionary() noexcept;
             void initDecompressionDictionary() noexcept;
 
-            std::unordered_map<std::vector<unsigned char>, size_t, ByteVectorHash> dictionary;
-            std::unordered_map<size_t, std::vector<unsigned char>> decompressionDictionary;
+            std::unordered_map<size_t, unsigned int, NoHash> dictionary;
+            std::unordered_map<unsigned int, std::deque<unsigned char>> decompressionDictionary;
+
+            void calculateHashForElement(unsigned char newElement);
+            static std::uint32_t const inline magicNumber = 0x9e3779b9;
+            size_t currentHash = 0;
+            size_t prevHash = 0;
         };
     }
 }
--- src/compression/LZWCompression.cpp
+++ src/compression/LZWCompression.cpp
@@ -1,57 +1,73 @@
 #include "compression/LZWCompression.h"
 #include <unordered_map>
 #include <limits>
+#include <deque>
 
 cch::compression::LZWCompression::LZWCompression() noexcept
 {
     resetState();
 }
 
-std::vector<size_t> cch::compression::LZWCompression::compress(std::span<unsigned char> data)
+std::vector<unsigned int> cch::compression::LZWCompression::compress(std::span<unsigned char> data)
 {
-    std::vector<size_t> result;
-    std::vector<unsigned char> currentByteSeq;
+    std::vector<unsigned int> result;
+    result.reserve(data.size() / sizeof(unsigned int));
+
+    std::deque<unsigned char> currentByteSeq;
     int code = dictionary.size();
+
+    size_t i = 0;
 
     for (auto x : data)
     {
         currentByteSeq.push_back(x);
 
-        if (dictionary.find(currentByteSeq) == dictionary.end())
+        calculateHashForElement(x);
+
+        if (dictionary.find(currentHash) == dictionary.end())
         {
-            auto bytePrefix = currentByteSeq;
+            auto bytePrefix = std::move(currentByteSeq);
             bytePrefix.pop_back();
-            result.push_back(dictionary[bytePrefix]);
-            dictionary[currentByteSeq] = code++;
-            currentByteSeq = {x};
+            result.push_back(dictionary[prevHash]);
+            dictionary[currentHash] = code++;
+
+            currentByteSeq.clear();
+            currentByteSeq.push_back(x);
+
+            currentHash = 0;
+            calculateHashForElement(x);
         }
+
+        prevHash = currentHash;
+        ++i;
     }
 
     if (!currentByteSeq.empty())
     {
-        result.push_back(dictionary[currentByteSeq]);
+        result.push_back(dictionary[currentHash]);
     }
 
+    result.shrink_to_fit();
     return result;
 }
 
-std::vector<unsigned char> cch::compression::LZWCompression::decompress(std::span<size_t> data)
+std::vector<unsigned char> cch::compression::LZWCompression::decompress(std::span<unsigned int> data)
 {
     std::vector<unsigned char> result;
     int code = decompressionDictionary.size();
 
-    std::vector<unsigned char> currentByteSeq = decompressionDictionary[data[0]];
+    auto currentByteSeq = decompressionDictionary[data[0]];
 
-    auto appendVector = [](auto &dst, auto& src)
-    {
-        dst.insert(dst.end(), src.begin(), src.end());
-    };
+    auto appendVector = [](auto& dst, auto& src)
+        {
+            dst.insert(dst.end(), src.begin(), src.end());
+        };
 
     appendVector(result, currentByteSeq);
 
     for (size_t i = 1; i < data.size(); ++i)
     {
-        std::vector<unsigned char> entry;
+        decltype(currentByteSeq) entry;
 
         if (auto it = decompressionDictionary.find(data[i]); it != decompressionDictionary.end())
         {
@@ -86,8 +102,12 @@ void cch::compression::LZWCompression::initCompressionDictionary() noexcept
 
     for (unsigned int i = 0; i <= std::numeric_limits<unsigned char>::max(); ++i)
     {
-        dictionary[std::vector<unsigned char>{static_cast<unsigned char>(i)}] = i;
+        currentHash = 0;
+        calculateHashForElement(i);
+        dictionary[currentHash] = i;
     }
+
+    currentHash = 0;
 }
 
 void cch::compression::LZWCompression::initDecompressionDictionary() noexcept
@@ -96,6 +116,11 @@ void cch::compression::LZWCompression::initDecompressionDictionary() noexcept
 
     for (unsigned int i = 0; i <= std::numeric_limits<unsigned char>::max(); ++i)
     {
-        decompressionDictionary[i] = {static_cast<unsigned char>(i)};
+        decompressionDictionary[i] = { static_cast<unsigned char>(i) };
     }
+}
+
+void cch::compression::LZWCompression::calculateHashForElement(unsigned char newElement)
+{
+    currentHash = std::hash<size_t>()(currentHash ^ std::hash<unsigned char>()(newElement));
 }
